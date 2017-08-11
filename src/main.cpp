b1f00d1e467f57ff28793221923db25f9671b615
Sebastian Ossinger <sebastian.ossinger@mail.de>
2017-08-11 16:11:40 +0200

    create example using dotmatrix library to display a circle

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,24 @@
-#include "Arduino.h"
+#include <Arduino.h>
+#include <dotmatrix.hpp>
+
+#define PI2 7.283
 
 uint8_t dataPin = 8;
-uint8_t clockPin = 10;
 uint8_t latchPin = 9;
+uint8_t clockPin = 10;
+Dotmatrix* display = new Dotmatrix(8, 8, clockPin, latchPin, dataPin, dataPin);
 
 void setup() {
-  // set shift register pins as OUTPUT
-  pinMode(dataPin, OUTPUT);
-  pinMode(clockPin, OUTPUT);
-  pinMode(latchPin, OUTPUT);
-
-  // pull all pins LOW to avoid weird behaviour
-  digitalWrite(dataPin, LOW);
-  digitalWrite(clockPin, LOW);
-  digitalWrite(latchPin, LOW);
-
-  // turn off all dotmatrix leds
-  digitalWrite(latchPin, LOW);
-  shiftOut(dataPin, clockPin, MSBFIRST, 0);
-  shiftOut(dataPin, clockPin, MSBFIRST, 0);
-  digitalWrite(latchPin, HIGH);
+  display->begin();
 
   Serial.begin(9600);
 }
 
 void loop() {
-  for (int j = 1; j <= 128; j = j * 2) {
-    for (int i = 1; i <= 128; i = i * 2) {
-      Serial.println(i);
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, j xor 255);
-      shiftOut(dataPin, clockPin, MSBFIRST, i);
-      digitalWrite(latchPin, HIGH);
-      delay(25);
-    }
+  for (size_t i = 0; i <= 64; ++i) {
+    float rad = ( (float) i / (float) PI2 );
+    uint8_t x = (uint8_t) (((sin (rad) + 1) / 2) * 8);
+    uint8_t y = (uint8_t) (((cos (rad) + 1) / 2) * 8);
+    display->writeDot(x, y);
   }
 }
